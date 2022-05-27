e1ab06d971dce93b65082e1f80c296916d61fce5
James Stovold <james.stovold@gmail.com>
2022-05-27 11:20:58 +0100

    Building out python side

--- core/src/CDMSim.py
+++ core/src/CDMSim.py
@@ -1,21 +1,59 @@
-import turtle
+import turtle as robotTurtle
+import turtle2 as cdmTurtle
+import time
+import threading
 
-class CogSisTurtle(Turtle):
+class CogSisTurtle(robotTurtle.Turtle):
   def __init__(self):
     super.__init__()
   
-  
-  
-  
+class CDMEnv():
+  _screen = None
+  _cdm    = None
+  def __init__(self, scale = 1.0):
+    _screen = cdmTurtle.Screen().setup(width  = 140 * scale,
+				       height = 140 * scale, 
+				       startx = 250 * scale, 
+				       starty = None)
+    
 
 
 
 
+
+
+class RobotEnv():
+  _screen = None
+  _robot  = None
+  def __init__(self, scale = 1.0):
+    _screen = robotTurtle.Screen().setup(width  = 200 * scale, 
+					 height = 140 * scale, 
+					 startx = 25  * scale, 
+					 starty = None)
+    _robot = robotTurtle.Turtle()
+    
+
+
+
+
+
+def robotThread():
+  while(1):
+    time.sleep(1)
+
+def cdmThread():
+  while(1):
+    time.sleep(1)
 
 def main():
-  t = turtle.Turtle()
-  a = turtle.setup(width=140, height=140, startx=None, starty=None);
+  #  t1 = threading.Thread(target=robotThread)
+  #  t2 = threading.Thread(target=cdmThread)
+  screen_scale = 4.0
+  cdmenv   = CDMEnv(screen_scale);
+  robotenv = RobotEnv(screen_scale);
 
+  while (1):
+    time.sleep(1)
 
 if __name__ == "__main__":
   main()
--- core/src/cdm.py
+++ core/src/cdm.py
@@ -6,10 +6,61 @@
 ## date  : May 26, 2022
 ## ==
 
-
 from ctypes import *
+class pyCDM():
+  _cdm = None
+  _lib  = None
+
+  def __init__(self):
+    self._lib = cdll.LoadLibrary('../../native/target/libCDMSim.so')
+    self._lib.createCDM.restype 	= POINTER(c_int)
+    self._lib.setRgbReadings.restype = c_bool 
+    self._lib.getRgbReadings.restype = POINTER(c_int * 3)
+    self._lib.getChargeTemp.restype  = POINTER(c_float * 2)
+    self._lib.tick.restype		= c_bool
+    self._lib.destroyCDM.restype	= c_bool
+    self._cdm = self._lib.createCDM()
+
+  def setRgbReadings(self, r, g, b):
+    return(self._lib.setRgbReadings(self._cdm, r, g, b))
+
+  def getRgbReadings(self):
+    rgb_ptr = self._lib.getRgbReadings(self._cdm)
+    returnList = [i for i in rgb_ptr.contents]
+    return(returnList)
+  
+  def getChargeTemp(self):
+    ct_ptr = self._lib.getChargeTemp(self._cdm)
+    returnList = [i for i in ct_ptr.contents]
+    return(returnList)
+    
+  def tick(self):
+    return(self._lib.tick(self._cdm))
+
+  def __del__(self):
+    self._lib.destroyCDM(self._cdm)
+    _cdm = 0;
+
 
 def main():
+
+ cdm = pyCDM()
+ print(cdm)
+ print(cdm.getRgbReadings())
+ cdm.setRgbReadings(500,213,1)
+ print(cdm.getChargeTemp())
+ print(cdm.tick())
+ print(cdm.getChargeTemp())
+ print(cdm.tick())
+ print(cdm.getChargeTemp())
+ print(cdm.tick())
+ print(cdm.getChargeTemp())
+ print(cdm.tick())
+ print(cdm.getChargeTemp())
+ del cdm
+
+ if False:
+
   lib = cdll.LoadLibrary('../../native/target/libCDMSim.so')
   lib.createCDM.restype 	= POINTER(c_int)
   lib.setRgbReadings.restype 	= c_bool 
